23f9b2828e80da9982c10d4d1f7290f710c418cf
Lukas Duncan <lukasduncan7@gmail.com>
2024-11-09 00:45:13 +0100

    added tableau

--- ui.js
+++ ui.js
@@ -1,1 +1,43 @@
-let gImage = document.getElementsByClassName("box");
+const modal = document.querySelector(".modal");
+const artPanel = document.querySelectorAll(".artPanel");
+
+if(artPanel) {
+    artPanel.forEach(image => {
+        image.addEventListener('click', () => {
+            modal.showModal();
+        })
+    });
+    
+
+// }
+
+// const galleryImages = document.querySelectorAll(".artPanel");
+// const modal = document.querySelector(".modal");
+
+// if (galleryImages) {
+//   galleryImages.forEach((image) => {
+//     image.addEventListener("click", () => {
+//         modal.style.display = "block";
+//         const enlargedImage = document.createElement("img");
+//         enlargedImage.src = image.src;
+//         enlargedImage.classList.add("enlargedImage");
+//         modal.appendChild(enlargedImage);
+//     });
+//   });
+// }
+
+// if (modal) {
+//   modal.addEventListener("click", (event) => {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//         modal.innerHTML = "";
+//     }
+//   });
+
+//   document.addEventListener('keydown', (event) => {
+//     if (modal && event.key === 'Escape') {
+//         modal.style.display = "none";
+//         modal.innerHTML = "";
+//     }
+//   });
+// }

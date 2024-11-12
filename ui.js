60e6a4199c1a760b2a582c7094eb78dab137174a
Lukas Duncan <lukasduncan7@gmail.com>
2024-11-12 23:17:32 +0100

    adding modal

--- ui.js
+++ ui.js
@@ -1,43 +1,30 @@
+const galleryImages = document.querySelectorAll(".artPanel");
 const modal = document.querySelector(".modal");
-const artPanel = document.querySelectorAll(".artPanel");
 
-if(artPanel) {
-    artPanel.forEach(image => {
-        image.addEventListener('click', () => {
-            modal.showModal();
-        })
+if (galleryImages) {
+  galleryImages.forEach((image) => {
+    image.addEventListener("click", () => {
+        modal.style.display = "block";
+        const enlargedImage = document.createElement("img");
+        enlargedImage.src = image.src;
+        enlargedImage.classList.add("enlargedImage");
+        modal.appendChild(enlargedImage);
     });
-    
+  });
+}
 
-// }
+if (modal) {
+    modal.addEventListener("click", (event) => {
+      if (event.target === modal) {
+          modal.style.display = "none";
+          modal.innerHTML = "";
+      }
+    });
 
-// const galleryImages = document.querySelectorAll(".artPanel");
-// const modal = document.querySelector(".modal");
-
-// if (galleryImages) {
-//   galleryImages.forEach((image) => {
-//     image.addEventListener("click", () => {
-//         modal.style.display = "block";
-//         const enlargedImage = document.createElement("img");
-//         enlargedImage.src = image.src;
-//         enlargedImage.classList.add("enlargedImage");
-//         modal.appendChild(enlargedImage);
-//     });
-//   });
-// }
-
-// if (modal) {
-//   modal.addEventListener("click", (event) => {
-//     if (event.target === modal) {
-//         modal.style.display = "none";
-//         modal.innerHTML = "";
-//     }
-//   });
-
-//   document.addEventListener('keydown', (event) => {
-//     if (modal && event.key === 'Escape') {
-//         modal.style.display = "none";
-//         modal.innerHTML = "";
-//     }
-//   });
-// }
+    document.addEventListener('keydown', (event) => {
+        if (modal && event.key === 'Escape') {
+            modal.style.display = "none";
+            modal.innerHTML = "";
+        }
+      });
+    }
